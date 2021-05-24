84bef1a47e68b04773e82c8267809ede7b679d69
TestardR <romain.rtestard@gmail.com>
2021-05-24 09:04:34 +0200

    feat(update-user) add update query

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -8,8 +8,10 @@ import (
 func mapUrls() {
 	router.GET("/ping", ping.Ping)
 
-	router.GET("/users/:user_id", users.GetUser)
-	router.POST("/users/search", users.SearchUser)
 	router.POST("/users", users.CreateUser)
+	router.GET("/users/:user_id", users.GetUser)
+	router.PUT("/users/:user_id", users.UpdateUser)
+	router.PATCH("/users/:user_id", users.UpdateUser)
+	router.POST("/users/search", users.SearchUser)
 
 }
